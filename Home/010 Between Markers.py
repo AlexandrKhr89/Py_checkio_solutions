8d4d1ec5a3b2f9b53afe717a616018c393024824
AlexandrKhr89 <alexandrkhr89@gmail.com>
2021-02-24 22:15:38 +0200

    Task 010 add to Home station

--- Home/010 Between Markers.py
+++ Home/010 Between Markers.py
@@ -5,14 +5,15 @@ But there are a few important conditions:
 
 The initial and final markers are always different.
 If there is no initial marker, then the first character should be considered the beginning of a string.
-
 If there is no final marker, then the last character should be considered the ending of a string.
-
 If the initial and final markers are missing then simply return the whole string.
 
 If the final marker comes before the initial marker, then return an empty string.
 
-Input: Three arguments. All of them are strings. The second and third arguments are the initial and final markers.
+Input:
+Three arguments.
+All of them are strings.
+The second and third arguments are the initial and final markers.
 
 Output: A string.
 
@@ -27,6 +28,28 @@ Precondition: can't be more than one final marker and can't be more than one ini
 '''
 
 
+# def between_markers(text: str, begin: str, end: str) -> str:
+#     """
+#         returns substring between two given markers
+#     """
+#     # your code here
+#     begin_index = text.find(begin) + 1
+#     end_index = text.find(end)
+#
+#     if end_index < begin_index:
+#         new_text = ''
+#     elif begin_index < end_index:
+#         new_text = text[begin_index:end_index]
+#
+#     print()
+#     print('text:', text)
+#     print('begin:', begin, 'begin_index:', begin_index)
+#     print('end:', end, 'end_index:', end_index)
+#     print('\nnew text:', new_text)
+#
+#     return new_text
+
+
 def between_markers(text: str, begin: str, end: str) -> str:
     """
         returns substring between two given markers
@@ -39,18 +62,26 @@ def between_markers(text: str, begin: str, end: str) -> str:
     i_e = t.find(e)
     new_text = ''
 
-    if i_b == -1:
-        new_text = t[:i_e]
-    elif i_e == -1:
-        new_text = t[i_b + len(b):]
-    elif i_b == -1 and i_e == -1:
+    if i_b == -1 and i_e == -1:
+        print('\n_1_')
         new_text = t
+
+    elif i_b != -1 and i_e == -1:
+        print('\n_2_')
+        new_text = t[i_b + len(b):]
+
+    elif i_b == -1 and i_e != -1:
+        print('\n_3_')
+        new_text = t[:i_e]
+
     elif i_b > i_e:
+        print('\n_4_')
         new_text = ''
     else:
+        print('\n_5_')
         new_text = t[i_b + len(b):i_e]
 
-    print('\ntext:', t)
+    print('text:', t)
     print('begin:', b)
     print('end:', e)
     # print('i_b:', i_b)
@@ -64,7 +95,9 @@ def between_markers(text: str, begin: str, end: str) -> str:
 
 if __name__ == '__main__':
     print('Example:')
-    print(between_markers('What is >apple<', '>', '<'))
+    print(between_markers('1 What is >apple<', '>', '<'))
+    print(between_markers('2 What is >apple<', '<', '>'))
+    print(between_markers('3 What is >apple<', '>', '<'))
 
     # These "asserts" are used for self-checking and not for testing
     assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
