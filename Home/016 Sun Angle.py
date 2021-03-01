47342d9e013289fdd990a95f4aa17ac31762c0c9
AlexandrKhr89 <alexandrkhr89@gmail.com>
2021-03-01 23:37:50 +0200

    Solution for Task 016 add to Home station

--- Home/016 Sun Angle.py
+++ Home/016 Sun Angle.py
@@ -31,15 +31,41 @@ Precondition:
 00:00 <= time <= 23:59
 '''
 
+
 def sun_angle(time):
-    #replace this for solution
-    return time
+    # replace this for solution
+    print('\ntime:', time)
+    # print('time[:2]:', time[:2])
+    # print('time[2:]:', time[3:])
+    hour = int(time[:2])
+    minuts = int(time[3:])
+    time_in_minut = hour * 60 + minuts
+    print('hour:', hour)
+    print('minuts:', minuts)
+    print('time_in_minut:', time_in_minut)
+
+    if time_in_minut >= 6 * 60 and time_in_minut <= 18 * 60:
+        angle = (180 / (12 * 60)) * (time_in_minut - 6 * 60)
+        print('angle:', angle)
+    else:
+        angle = "I don't see the sun!"
+        print('angle:', angle)
+
+    return angle
+
+
+def sun_angle(time):
+    if (6 <= (int(time[:2]) + int(time[3:]) / 60) <= 18):
+        return (180 / 12) * (int(time[:2]) + (int(time[3:]) / 60) - 6)
+    else:
+        return "I don't see the sun!"
+
 
 if __name__ == '__main__':
     print("Example:")
     print(sun_angle("07:00"))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert sun_angle("07:00") == 15
     assert sun_angle("01:23") == "I don't see the sun!"
     print("Coding complete? Click 'Check' to earn cool rewards!")
